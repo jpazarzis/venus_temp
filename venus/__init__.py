7e572de5a6bdb6f2a00b73e832175490f4b99bea
jpazarzis <jpazarzis@gmail.com>
2018-02-11 13:15:49 -0500

    Adding tests.

--- venus/__init__.py
+++ venus/__init__.py
@@ -39,7 +39,6 @@ called health_checks) similar to the following::
 
     >>> def check_redis(server, passwd):
     >>>    # Apply your checks and return a dict
-    ... # doctest: +SKIP
 
 You can now use this library as follows::
 
